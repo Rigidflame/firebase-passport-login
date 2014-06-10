b8f2bad2b68bdd6c508ccae6953cdd276cc825ee
Abraham Haskins <abeisgreat@abeisgreat.com>
2014-06-10 09:54:03 -0700

    Cleans up server code

--- server/server.js
+++ server/server.js
@@ -7,40 +7,40 @@ var Firebase = require('firebase');
 var app = express();
 
 app.configure(function() {
-  app.use(express.static('public'));
-  app.use(express.cookieParser(serverConfig.COOKIE_SECRET));
-  app.use(express.bodyParser());
-  app.use(express.session({ secret: serverConfig.COOKIE_SECRET }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
+    //app.use(express.static('public'));
+    app.use(express.cookieParser(serverConfig.COOKIE_SECRET));
+    app.use(express.bodyParser());
+    app.use(express.session({ secret: serverConfig.COOKIE_SECRET }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
 });
 
-var ref = new Firebase(serverConfig.FIREBASE_URL)
-var tokGen = new FirebaseTokenGenerator(serverConfig.FIREBASE_SECRET)
+var tokGen = new FirebaseTokenGenerator(serverConfig.FIREBASE_SECRET);
+var port = Number(process.env.PORT || 5000);
 
-ref.auth(serverConfig.FIREBASE_SECRET, function (err, data) {
-    if (err) throw err;
-    
-    serverConfig.SERVICES.forEach(function (service) {
-        var serviceObject = require('./services/' + service).setup(passport);
-        
-        app.get('/auth/' + service, function(req, res, next){
-            res.cookie('passportAnonymous', req.query.oAuthTokenPath, {signed: true});
-            passport.authenticate(service, serviceObject.options)(req, res, next);
-        });
+serverConfig.SERVICES.forEach(function (service) {
+    var serviceObject = require('./services/' + service).setup(passport);
 
-        app.get('/auth/' + service + '/callback', function (req, res, next) {
-          passport.authenticate(service, function(err, user, info) {
-                ref.child('oAuthToken').child(user.uid).set(user.accessToken)
-                var tok = null;
-                if( !err && user ) {
-                    tok = tokGen.createToken(user);
-                }
-                ref.child(req.signedCookies.passportAnonymous).set(tok);
-            })(req, res, next);
-        });
+    app.get('/auth/' + service, function(req, res, next){
+        res.cookie('passportAnonymous', req.query.oAuthTokenPath, {signed: true});
+        passport.authenticate(service, serviceObject.options)(req, res, next);
     });
 
-    app.listen(1337);
-})
+    app.get('/auth/' + service + '/callback', function (req, res, next) {
+        var ref = new Firebase(serverConfig.FIREBASE_URL)
+        passport.authenticate(service, function(err, user, info) {
+            ref.auth(serverConfig.FIREBASE_SECRET, function (err, data) {
+                ref.child('oAuthToken').child(user.uid).set(user.accessToken)
+                var tok = null;
+                if( user ) {
+                    tok = tokGen.createToken(user);
+                }
+
+                ref.child(req.signedCookies.passportAnonymous).set(tok);
+            })
+        })(req, res, next);
+    });
+});
+
+app.listen(port);
